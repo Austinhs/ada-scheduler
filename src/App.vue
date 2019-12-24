07d8273ac889d99924ec841e365c8c2e8d4cc974
Austin Stanfield <austin_h_s@hotmail.com>
2019-12-24 02:01:36 -0500

    Add utc time

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,36 @@
 <template>
   <div id="app">
     <div>Yo we got this: {{stats}}</div>
+    <div>Yo the UTC is: {{utc}}</div>
   </div>
 </template>
 
 <script>
 const BACKEND_URL = window.location.hostname;
+
 export default {
   name: 'app',
   data() {
     return {
-      stats: null
+      stats: null,
+      utc: null
     }
   },
 
-  methods: {
-    async getStats() {
-      const res = await fetch(`${BACKEND_URL}:2000/api/stats`);
+  async created() {
+    const res = await fetch(`http://${BACKEND_URL}:2000/api/stats`);
+    const { data } = await res.json();
 
-      this.stats = await res.json();
-    }
+    this.stats = data;
+
+    setInterval(() => {
+      var date  = new Date();
+      var hours = date.getUTCHours();
+      var mins  = date.getUTCMinutes();
+      var secs  = date.getUTCSeconds();
+
+      this.utc = `${hours}:${mins}:${secs}`;
+    }, 500);
   }
 }
 </script>
--- src/server/route.js
+++ src/server/route.js
@@ -2,9 +2,17 @@ const express = require('express');
 const app = express();
 const port = 2000;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/api/stats', (req, res) => {
     // get the stat;
-    res.send('Testing');
+    res.send({
+        data: 'testing'
+    });
 });
 
 app.listen(port, () => console.log(`Running port: ${port}`));
