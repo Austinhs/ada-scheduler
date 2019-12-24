229feff03c34efebc227901ace0de81ef2c59ca7
Austin Stanfield <austin_h_s@hotmail.com>
2019-12-24 02:11:18 -0500

    Adding vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,60 @@
 <template>
-  <div id="app">
-    <div>Yo we got this: {{stats}}</div>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-const BACKEND_URL = window.location.hostname;
+import HelloWorld from './components/HelloWorld';
+
 export default {
-  name: 'app',
-  data() {
-    return {
-      stats: null
-    }
+  name: 'App',
+
+  components: {
+    HelloWorld,
   },
 
-  methods: {
-    async getStats() {
-      const res = await fetch(`${BACKEND_URL}:2000/api/stats`);
-
-      this.stats = await res.json();
-    }
-  }
-}
+  data: () => ({
+    //
+  }),
+};
 </script>
